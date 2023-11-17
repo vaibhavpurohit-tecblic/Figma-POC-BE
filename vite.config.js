ccecb0f046b667946878324344dcf15cb7973c18
Vibhu0412 <vbpurohit1948@gmail.com>
2023-11-17 19:46:32 +0530

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
+const isProduction = process.env.NODE_ENV === "production";
+const herokuDomain = "https://zdai-ad-copy-745906f359ba.herokuapp.com";
+
 export default defineConfig({
-  base: process.env.ENV === "prod" ? "https://zdai-ad-copy-745906f359ba.herokuapp.com/" : "/",
+  base: isProduction ? `/${herokuDomain}/` : "/",
   plugins: [vue()],
   server: {
+    port: isProduction ? 5173 : 4173,
     proxy: {
       "/api": {
-        target: "http://localhost:5000", // Replace with your API base URL
+        target: isProduction ? herokuDomain : "http://localhost:5000",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
