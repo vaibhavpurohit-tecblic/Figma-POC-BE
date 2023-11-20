a8d92334eead9bc329bfbd9a9a22fc40a53c696a
AasimMalikTecblic <aasim.malik@tecblic.com>
2023-11-20 17:09:13 +0530

    config change

--- vite.config.js
+++ vite.config.js
@@ -33,7 +33,7 @@ export default ({ mode }) => {
       proxy: {
         "/api": {
           target: isProduction
-            ? "http://0.0.0.0:5000"
+            ? "http://0.0.0.0:" + process.env.DYNO?.split("run.")?.[1] || "5000"
             : "http://localhost:5000",
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ""),
