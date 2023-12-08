453b57565129c75d57915b189e1f6caf5afd3095
AasimMalikTecblic <aasim.malik@tecblic.com>
2023-12-08 17:42:50 +0530

    SideBar Date Listing Done

--- src/components/General/Sidebar.vue
+++ src/components/General/Sidebar.vue
@@ -3,6 +3,7 @@ import { onMounted, ref } from "vue";
 import SidebarTitle from "./SidebarTitle.vue";
 import { AdCopyListApiFunction } from "../../api/AdCopyApis/index.js";
 import { ExpertBotListApiFunction } from "../../api/ExpertBotApis/index.js";
+import moment from "moment";
 
 const props = defineProps({
   title: String,
@@ -29,11 +30,53 @@ function RedirectLinkFunction() {
   }
 }
 
+function ChatDataSortingFunction(data) {
+  var tempArray = [
+    { title: "Today", chat: [] },
+    { title: "Tomorrow", chat: [] },
+    { title: "Past 7 Days", chat: [] },
+    { title: "Past 30 Days", chat: [] },
+    { title: "Later Than 30 Days", chat: [] },
+  ];
+
+  var sortChat = data.sort(function (a, b) {
+    return (
+      moment(b.createdAt, "ddd, DD MMM YYYY HH:mm:ss [GMT]").valueOf() -
+      moment(a.createdAt, "ddd, DD MMM YYYY HH:mm:ss [GMT]").valueOf()
+    );
+  });
+
+  const currentDate = moment().format("LL");
+
+  const tomorrowDate = moment(moment().subtract(1, "days")).format("LL");
+
+  const weekDate = moment(moment().subtract(7, "days")).format("LL");
+
+  const monthDate = moment(moment().subtract(30, "days")).format("LL");
+
+  for (let i = 0; i < sortChat.length; i++) {
+    var tempDate = moment(sortChat[i].createdAt).format("LL");
+    if (moment(tempDate).isSame(currentDate)) {
+      tempArray[0].chat = [...tempArray[0].chat, sortChat[i]];
+    } else if (moment(tempDate).isSame(tomorrowDate)) {
+      tempArray[1].chat = [...tempArray[1].chat, sortChat[i]];
+    } else if (moment(tempDate).isAfter(weekDate)) {
+      tempArray[2].chat = [...tempArray[2].chat, sortChat[i]];
+    } else if (moment(tempDate).isAfter(monthDate)) {
+      tempArray[3].chat = [...tempArray[3].chat, sortChat[i]];
+    } else {
+      tempArray[4].chat = [...tempArray[4].chat, sortChat[i]];
+    }
+  }
+
+  sideBarChatList.value = tempArray?.filter((item) => item?.chat?.length > 0);
+}
+
 async function AdCopyChatListFunction() {
   const result = await AdCopyListApiFunction();
 
   if (result.status === 200) {
-    sideBarChatList.value = result?.data?.chats || [];
+    ChatDataSortingFunction(result?.data?.chats || []);
   } else {
     sideBarChatList.value = [];
   }
@@ -43,7 +86,7 @@ async function ExpertBotChatListFunction() {
   const result = await ExpertBotListApiFunction();
 
   if (result.status === 200) {
-    sideBarChatList.value = result?.data?.chats || [];
+    ChatDataSortingFunction(result?.data?.chats || []);
   } else {
     sideBarChatList.value = [];
   }
@@ -89,13 +132,15 @@ onMounted(() => {
         class="flex flex-col gap-3 h-[calc(100vh-105px)] md:h-[calc(100vh-260px)] overflow-auto"
         v-if="props.sidebarClose"
       >
-        <p class="text-sm text-gray-500 font-normal">Today</p>
-        <div
-          class="flex flex-col gap-3"
-          v-for="item in sideBarChatList"
-          :key="item.id"
-        >
-          <SidebarTitle :title="item.title" :id="item.id" />
+        <div class="" v-for="item in sideBarChatList" :key="item.id">
+          <p class="text-sm text-gray-500 font-normal">{{ item.title }}</p>
+          <div
+            class="flex flex-col gap-3"
+            v-for="item2 in item.chat"
+            :key="item2.id"
+          >
+            <SidebarTitle :title="item2.title" :id="item2.id" />
+          </div>
         </div>
       </div>
     </div>
