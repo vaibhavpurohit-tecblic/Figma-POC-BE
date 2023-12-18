8a3788bf2d9c14d73ba4751d94fd102eadf9ff50
Vibhu0412 <vbpurohit1948@gmail.com>
2023-12-18 11:11:57 +0530

    implement live redis endpoint

--- app/celery_config.py
+++ app/celery_config.py
@@ -2,8 +2,8 @@ from celery import Celery
 
 celery = Celery(
     'app',
-    broker='redis://localhost:6379/0',
-    backend='redis://localhost:6379/0',
+    broker='redis://default:Ygcj1FxTyYPXlkpImP4zxw1QTNiHBvkH@redis-11437.c277.us-east-1-3.ec2.cloud.redislabs.com:11437',
+    backend='redis://default:Ygcj1FxTyYPXlkpImP4zxw1QTNiHBvkH@redis-11437.c277.us-east-1-3.ec2.cloud.redislabs.com:11437',
 )
 
 celery.conf.update(
