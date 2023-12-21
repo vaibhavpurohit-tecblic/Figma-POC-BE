2eaaac4d5b001ed98ae2d5d0c13cfd30a0f8540d
AasimMalikTecblic <aasim.malik@tecblic.com>
2023-12-21 11:45:03 +0530

    Chat Title Edit and Chat Delete UI added

--- src/components/General/SidebarTitle.vue
+++ src/components/General/SidebarTitle.vue
@@ -5,6 +5,7 @@ import {
   GetPageSearch,
   RedirectPage,
 } from "../Constants/index.js";
+import DeleteModal from "./DeleteModal.vue";
 
 const props = defineProps({
   title: String,
@@ -27,6 +28,38 @@ function RedirectToPage() {
   }
 }
 
+const editActive = ref(false);
+
+const changeTitle = ref("");
+
+function EditFlagChange() {
+  editActive.value = true;
+  changeTitle.value = props.title;
+}
+
+function EditFlagCloseChange() {
+  editActive.value = false;
+  changeTitle.value = "";
+}
+
+function TitleChangeFunction(e) {
+  changeTitle.value = e.target.value;
+}
+
+function EditTitle() {}
+
+const deleteModalActive = ref(false);
+
+function DeleteModalFlagChange() {
+  deleteModalActive.value = true;
+}
+
+function DeleteModalFlagCloseChange() {
+  deleteModalActive.value = false;
+}
+
+function DeleteChatFunction() {}
+
 onMounted(() => CheckIfIdMatch());
 </script>
 
@@ -41,41 +74,71 @@ onMounted(() => CheckIfIdMatch());
     @click="() => RedirectToPage()"
   >
     <div class="">
+      <input
+        type="text"
+        class="w-full bg-transparent focus-visible:outline-none text-white text-base font-medium border-b border-b-white"
+        :value="changeTitle"
+        @input="TitleChangeFunction"
+        v-if="editActive"
+      />
       <h6
         :class="{
           'text-primary text-base font-medium': !active,
           'text-white text-base font-medium': active,
         }"
+        v-else
       >
         {{ props.title }}
       </h6>
     </div>
-    <div class="flex gap-3 flex-none">
+    <div class="flex gap-3 flex-none" v-if="editActive">
+      <img
+        src="../../assets/logos/check.svg"
+        alt="Edit Icon"
+        class="w-5 h-5 cursor-pointer"
+        @click="() => EditTitle()"
+      />
+      <img
+        src="../../assets/logos/close.svg"
+        alt="Delete Icon"
+        class="w-4 h-4 cursor-pointer"
+        @click="() => EditFlagCloseChange()"
+      />
+    </div>
+    <div class="flex gap-3 flex-none" v-else>
       <img
         src="../../assets/logos/editActive.svg"
         alt="Edit Icon"
-        class="w-4 h-4 cursor-pointer"
+        class="w-5 h-5 cursor-pointer"
         v-if="active"
+        @click="() => EditFlagChange()"
       />
       <img
         src="../../assets/logos/editInActive.svg"
         alt="Edit Icon"
-        class="w-4 h-4"
+        class="w-5 h-5"
         v-else
       />
       <img
         src="../../assets/logos/deleteActive.svg"
         alt="Delete Icon"
-        class="w-4 h-4 cursor-pointer"
+        class="w-5 h-5 cursor-pointer"
         v-if="active"
+        @click="() => DeleteModalFlagChange()"
       />
       <img
         src="../../assets/logos/deleteInActive.svg"
         alt="Delete Icon"
-        class="w-4 h-4"
+        class="w-5 h-5"
         v-else
       />
     </div>
+    <DeleteModal
+      :chat="props.title"
+      :deleteModalActive="deleteModalActive"
+      :deleteChatFunction="DeleteChatFunction"
+      :deleteModalFlagCloseChange="DeleteModalFlagCloseChange"
+    />
   </div>
 </template>
 
