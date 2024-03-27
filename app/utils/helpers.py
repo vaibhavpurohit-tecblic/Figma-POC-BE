35f8aa7d8ab26b63b4aa3f798bee98a3914f2cd8
Vibhu0412 <vbpurohit1948@gmail.com>
2024-03-27 14:11:44 +0530

    update content-type

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -1,5 +1,9 @@
-import boto3
 import os
+
+import boto3
+import dotenv
+
+dotenv.load_dotenv(".env")
 
 ACCESS_KEY = os.getenv('AWS_ACCESS_KEY')
 SECRET_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
@@ -11,18 +15,36 @@ s3 = boto3.client('s3',
                   aws_secret_access_key=SECRET_KEY,
                   region_name=REGION_NAME)
 
-def upload_directory(directory_name, bucket_name):
-    for root, dirs, files in os.walk(directory_name):
+
+def get_content_type(file_path):
+    file_extension = os.path.splitext(file_path)[1].lower()
+    if file_extension == '.svg':
+        return 'image/svg+xml'
+    elif file_extension == '.png':
+        return 'image/png'
+    elif file_extension == '.jpg' or file_extension == '.jpeg':
+        return 'image/jpeg'
+    elif file_extension == '.html':
+        return 'text/html'
+    else:
+        return 'application/octet-stream'
+
+
+def upload_directory(local_directory, bucket_name, s3_prefix=''):
+    for root, dirs, files in os.walk(local_directory):
         for file in files:
             file_path = os.path.join(root, file)
-            # Remove the directory_name prefix and add 'development/' as the prefix
-            s3_file_key = f"development/{file_path.replace(directory_name, '', 1).lstrip('/')}"
+            s3_file_key = f"{s3_prefix}{file_path.replace(local_directory, '', 1).lstrip('/')}"
+            content_type = get_content_type(file_path)
             try:
-                s3.upload_file(file_path, bucket_name, s3_file_key)
-                print(f'File "{file_path}" uploaded successfully to bucket "{bucket_name}" as "{s3_file_key}"')
+                s3.upload_file(file_path, bucket_name, s3_file_key, ExtraArgs={'ContentType': content_type})
+                print(
+                    f'File "{file_path}" uploaded successfully to bucket "{bucket_name}" as "{s3_file_key}" with content type "{content_type}"')
             except Exception as e:
                 print(f'Error uploading file "{file_path}": {str(e)}')
 
+
 if __name__ == '__main__':
-    directory_name = 'app/output' 
-    upload_directory(directory_name, BUCKET_NAME)
+    local_path = 'app/output'
+    s3_path = 'development/'
+    upload_directory(local_path, BUCKET_NAME, s3_prefix=s3_path)
